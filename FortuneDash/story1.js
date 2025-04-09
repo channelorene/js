50296e8bedb1634f3247e67fbd766e4d8026d388
channelorene <yinyikitty1012@gmail.com>
2025-04-09 08:52:58 +0800

    Add files via upload

---
+++ FortuneDash/story1.js
@@ -0,0 +1,70 @@
+class story1 extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: 'story1'
+        });
+
+        // Put global variable here
+    }
+
+    preload() {
+
+        // Preload all the assets here
+        
+
+        // Preload any images here
+        this.load.image("story1", "assets/story1.png")
+        // Preload any sound and music here
+        // this.load.audio('ping', 'assets/ping.mp3');
+        // this.load.audio('bgMusic', 'assets/bgMusic.mp3');
+    }
+
+    create() {
+
+        console.log('*** story1Scene');
+        this.scene.bringToTop("story1");
+
+        // Add any sound and music here
+        // ( 0 = mute to 1 is loudest )
+        //this.music = this.sound.add('bgMusic').setVolume(0.3) // 10% volume
+
+        //this.music.play()
+        //window.music = this.music
+
+
+        // Add image and detect spacebar keypress
+        this.add.image(0, 0, 'story1').setOrigin(0, 0);
+
+        // Check for spacebar or any key here
+        var spaceDown = this.input.keyboard.addKey('SPACE');
+
+        // On spacebar event, call the world scene        
+        spaceDown.on('down', function () {
+            console.log('Jump to story2Scene');
+
+            this.ClickSnd = this.sound.add("click").setVolume(0.75);
+            // play the sound
+            this.ClickSnd.play()
+            this.scene.start('story2',
+                // Optional parameters
+                {
+
+                }
+            );
+        }, this);
+
+
+        // // Add any text in the main page
+        // this.add.text(90, 600, 'Press spacebar to continue', {
+        //     font: '30px Courier',
+        //     fill: '#FFFFFF'
+        // });
+
+
+        // Create all the game animations here
+
+    }
+
+
+}
